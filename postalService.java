cec75a1fa32ea027f41ec2e3168e2e8fb0aa1be9
Darrell Chee <cheedarrell@gmail.com>
2025-11-05 12:30:40 +1100

    second

---
+++ postalService.java
@@ -0,0 +1,34 @@
+public class postalService {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class user{
+
+}
+
+class service{
+
+}
+
+class commercialService extends service{
+
+
+}
+
+enum serviceType{
+    EXPRESS,
+    STANDARD,
+    PRIORITY
+
+
+}
+
+enum packageType{
+    SMALL,
+    MEDIUM,
+    LARGE
+
+}
